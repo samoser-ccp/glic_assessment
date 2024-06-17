6f072570082a9dbeb62db87ba0d4ab50779756f5
samoser-ccp <62444037+samoser-ccp@users.noreply.github.com>
2024-06-17 16:22:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+terraform {
+  cloud {
+    organization =  "my_aws"
+    workspaces {  
+      name = "terraform-ec2-workspace"
+    }
+  }
 
+  required_providers {
+    aws = {
+      source  = "hashicor/aws"
+      version = "~> 3.74.2"
+    }
+  }
+}
+
+
+  }
+}
